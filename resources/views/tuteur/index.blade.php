283b3b1912099d0cd5b6e576f6edf71770846e83
Abdelali662 <abdelalimardi662@gmail.com>
2024-11-26 15:08:15 +0100

    Initial commit

---
+++ resources/views/tuteur/index.blade.php
@@ -0,0 +1,23 @@
+@extends('layout.nav')
+
+@section('contenue')
+<h1>Lise des produit</h1>
+
+<table class="table">
+    <thead>
+        <tr>
+            <th>id</th>
+            <th>Designation</th>
+            <th>Prix</th>
+            <th>stock</th>
+            <th>famille_id</th>
+            <th>Action</th>
+        </tr>
+    </thead>
+    <tbody>
+
+
+
+    </tbody>
+</table>
+@endsection
